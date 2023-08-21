5621f7d3865476b13897ae18fcc09a0480ded882
kaflamalice <kylecs.ka@gmail.com>
2023-08-21 13:04:20 +0200

    feat: add .gitignore for skeleoton spring project

---
+++ berlin-clock-api/src/main/java/fr/katanley/berlinclockapi/BerlinClockApiApplication.java
@@ -0,0 +1,13 @@
+package fr.katanley.berlinclockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerlinClockApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BerlinClockApiApplication.class, args);
+    }
+
+}
---
+++ berlin-clock-api/src/test/java/fr/katanley/berlinclockapi/BerlinClockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.katanley.berlinclockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerlinClockApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
