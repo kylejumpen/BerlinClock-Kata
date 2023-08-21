c16eef738e83dbb351bb097e8c8dd9d263cbfdd4
kaflamalice <kylecs.ka@gmail.com>
2023-08-21 13:03:21 +0200

    feat: Have skeleton ready

---
+++ berlin-clock-api/src/main/java/fr/katanley/berlinclockapi/service/time/ClockHelper.java
@@ -0,0 +1,108 @@
+package fr.katanley.berlinclockapi.service.time;
+
+import java.time.LocalTime;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static fr.katanley.berlinclockapi.service.time.Symbol.*;
+
+
+/**
+ * Here is the class that helps to convert a given time into the BerlinClock Representation
+ * @author kaflamalice
+ */
+public class ClockHelper {
+
+    public static final int SINGLE_MINUTES_ROW_LENGTH = 4;
+    public static final int SINGLE_HOURS_ROW_LENGTH = 4;
+    public static final int FIVE_MINUTES_ROW_LENGTH = 11;
+    public static final int FIVE_HOURS_ROWS_LENGTH = 4;
+
+    /**
+     *
+     * @param time is the time that we want to convert
+     * @return the single minute Row of the Berlin Clock, see
+     * @see fr.katanley.berlinclockapi.service.time.Symbol to change Light Symbol
+     */
+    public String getTheSingleMinutesRow(LocalTime time) {
+        int minute = time.getMinute();
+        int i = minute % 5;
+        if (i == 0) {
+            return IntStream.range(0, SINGLE_MINUTES_ROW_LENGTH).mapToObj((id) -> NO_LIGHT_SYMBOL.toString()).collect(Collectors.joining());
+        }
+        StringBuilder minutesRow;
+        minutesRow = new StringBuilder(YELLOW_LIGHT_SYMBOL.toString().repeat(i));
+        while (minutesRow.length() < SINGLE_MINUTES_ROW_LENGTH) {
+            minutesRow.append(NO_LIGHT_SYMBOL);
+        }
+        return minutesRow.toString();
+    }
+
+    public String getTheSingleHoursRow(LocalTime time) {
+        int hour = time.getHour();
+        int i = hour % 5;
+        if (i == 0) {
+            return IntStream.range(0, SINGLE_HOURS_ROW_LENGTH).mapToObj((id) -> NO_LIGHT_SYMBOL.toString()).collect(Collectors.joining());
+        }
+        StringBuilder hoursRow;
+        hoursRow = new StringBuilder(RED_LIGHT_SYMBOL.toString().repeat(i));
+        while (hoursRow.length() < SINGLE_HOURS_ROW_LENGTH) {
+            hoursRow.append(NO_LIGHT_SYMBOL);
+        }
+        return hoursRow.toString();
+    }
+
+    public String getTheFiveHoursRow(LocalTime time) {
+        int hours = time.getHour();
+        int i = hours / 5;
+        if (i == 0) {
+            return IntStream.range(0, FIVE_HOURS_ROWS_LENGTH).mapToObj((id) -> NO_LIGHT_SYMBOL.toString()).collect(Collectors.joining());
+        }
+        StringBuilder fiveHoursRow = new StringBuilder();
+        fiveHoursRow.append(RED_LIGHT_SYMBOL.toString().repeat(i));
+        while (fiveHoursRow.length() < FIVE_HOURS_ROWS_LENGTH) {
+            fiveHoursRow.append(NO_LIGHT_SYMBOL);
+        }
+        return fiveHoursRow.toString();
+    }
+
+    public String getTheFiveMinutesRow(LocalTime time) {
+        int minute = time.getMinute();
+        int i = minute / 5;
+        if (i == 0) {
+            return IntStream.range(0, FIVE_MINUTES_ROW_LENGTH).mapToObj((id) -> NO_LIGHT_SYMBOL.toString()).collect(Collectors.joining());
+        }
+        StringBuilder fiveMinutesRow = this.fillTheFiveMinuteRow(i);
+        while (fiveMinutesRow.length() < FIVE_MINUTES_ROW_LENGTH)
+            fiveMinutesRow.append(NO_LIGHT_SYMBOL);
+        return fiveMinutesRow.toString();
+    }
+
+    public String getTheSecondsLamp(LocalTime time) {
+        int second = time.getSecond();
+        if (second % 2 == 0) {
+            return YELLOW_LIGHT_SYMBOL.toString();
+        }
+        return NO_LIGHT_SYMBOL.toString();
+    }
+
+    public String getTheEntireClockRepresentation(LocalTime time) {
+        return getTheSecondsLamp(time)
+                .concat(getTheFiveHoursRow(time))
+                .concat(getTheSingleHoursRow(time))
+                .concat(getTheFiveMinutesRow(time))
+                .concat(getTheSingleMinutesRow(time));
+    }
+
+    private StringBuilder fillTheFiveMinuteRow(int quotient){
+        StringBuilder fiveMinutesRow = new StringBuilder();
+        for (int j = 1; j <= quotient; j++) {
+            if (j % 3 == 0) {
+                fiveMinutesRow.append(RED_LIGHT_SYMBOL);
+            } else {
+                fiveMinutesRow.append(YELLOW_LIGHT_SYMBOL);
+            }
+        }
+        return fiveMinutesRow;
+    }
+}
---
+++ berlin-clock-api/src/test/java/fr/katanley/berlinclockapi/service/time/ClockHelperTest.java
@@ -0,0 +1,148 @@
+package fr.katanley.berlinclockapi.service.time;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.time.LocalTime;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClockHelperTest {
+
+    @ParameterizedTest
+    @MethodSource("singleMinutesToTextRow")
+    public void shouldBeAbleToGetTheSingleMinuteRowsProperly(LocalTime time, String textRepresentation) {
+        //Given
+        ClockHelper clockHelper = new ClockHelper();
+
+        //When
+        String theSingleMinutesRow = clockHelper.getTheSingleMinutesRow(time);
+
+        //then
+        assertEquals(textRepresentation, theSingleMinutesRow);
+    }
+
+    private static Stream<Arguments> singleMinutesToTextRow() {
+        return Stream.of(
+                Arguments.arguments(LocalTime.of(0, 0, 0), "OOOO"),
+                Arguments.arguments(LocalTime.of(23, 59, 59), "YYYY"),
+                Arguments.arguments(LocalTime.of(12, 32, 0), "YYOO"),
+                Arguments.arguments(LocalTime.of(12, 34, 0), "YYYY"),
+                Arguments.arguments(LocalTime.of(12, 35, 0), "OOOO")
+        );
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("fiveMinutesToTextRow")
+    public void shouldBeAbleToGetTheFiveMinuteRowsProperly(LocalTime time, String textRepresentation) {
+        //Given
+        ClockHelper clockHelper = new ClockHelper();
+
+        //When
+        String theFiveMinutesRow = clockHelper.getTheFiveMinutesRow(time);
+
+        //then
+        assertEquals(textRepresentation, theFiveMinutesRow);
+    }
+
+    private static Stream<Arguments> fiveMinutesToTextRow() {
+        return Stream.of(
+                Arguments.arguments(LocalTime.of(0, 0, 0), "OOOOOOOOOOO"),
+                Arguments.arguments(LocalTime.of(23, 59, 59), "YYRYYRYYRYY"),
+                Arguments.arguments(LocalTime.of(12, 4, 0), "OOOOOOOOOOO"),
+                Arguments.arguments(LocalTime.of(12, 23, 0), "YYRYOOOOOOO"),
+                Arguments.arguments(LocalTime.of(12, 35, 0), "YYRYYRYOOOO")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("singleHoursToTextRow")
+    public void shouldBeAbleToGetTheSingleHoursRowsProperly(LocalTime time, String textRepresentation) {
+        //Given
+        ClockHelper clockHelper = new ClockHelper();
+
+        //When
+        String theSingleHoursRow = clockHelper.getTheSingleHoursRow(time);
+
+        //then
+        assertEquals(textRepresentation, theSingleHoursRow);
+    }
+
+    private static Stream<Arguments> singleHoursToTextRow() {
+        return Stream.of(
+                Arguments.arguments(LocalTime.of(0, 0, 0), "OOOO"),
+                Arguments.arguments(LocalTime.of(23, 59, 59), "RRRO"),
+                Arguments.arguments(LocalTime.of(2, 4, 0), "RROO"),
+                Arguments.arguments(LocalTime.of(8, 23, 0), "RRRO"),
+                Arguments.arguments(LocalTime.of(14, 35, 0), "RRRR")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("fiveHoursToTextRow")
+    public void shouldBeAbleToGetTheFiveHoursRowsProperly(LocalTime time, String textRepresentation) {
+        //Given
+        ClockHelper clockHelper = new ClockHelper();
+
+        //When
+        String theSingleHoursRow = clockHelper.getTheFiveHoursRow(time);
+
+        //then
+        assertEquals(textRepresentation, theSingleHoursRow);
+    }
+
+    private static Stream<Arguments> fiveHoursToTextRow() {
+        return Stream.of(
+                Arguments.arguments(LocalTime.of(0, 0, 0), "OOOO"),
+                Arguments.arguments(LocalTime.of(23, 59, 59), "RRRR"),
+                Arguments.arguments(LocalTime.of(2, 4, 0), "OOOO"),
+                Arguments.arguments(LocalTime.of(8, 23, 0), "ROOO"),
+                Arguments.arguments(LocalTime.of(16, 35, 0), "RRRO")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("secondsToLamp")
+    public void shouldBeAbleToGetTheSecondLampProperly(LocalTime time, String textRepresentation) {
+        //Given
+        ClockHelper clockHelper = new ClockHelper();
+
+        //When
+        String theSecondsLamp = clockHelper.getTheSecondsLamp(time);
+
+        //then
+        assertEquals(textRepresentation, theSecondsLamp);
+    }
+
+    private static Stream<Arguments> secondsToLamp() {
+        return Stream.of(
+                Arguments.arguments(LocalTime.of(0, 0, 0), "Y"),
+                Arguments.arguments(LocalTime.of(23, 59, 59), "O")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("fillTimeToTextRepresentation")
+    public void shouldBeAbleToGetTheEntireBerlinClock(LocalTime time, String textRepresentation) {
+        //Given
+        ClockHelper clockHelper = new ClockHelper();
+
+        //When
+        String theEntireClockRepresentation = clockHelper.getTheEntireClockRepresentation(time);
+
+        //then
+        assertEquals(textRepresentation, theEntireClockRepresentation);
+    }
+
+    private static Stream<Arguments> fillTimeToTextRepresentation() {
+        return Stream.of(
+                Arguments.arguments(LocalTime.of(0, 0, 0), "YOOOOOOOOOOOOOOOOOOOOOOO"),
+                Arguments.arguments(LocalTime.of(23, 59, 59), "ORRRRRRROYYRYYRYYRYYYYYY"),
+                Arguments.arguments(LocalTime.of(16, 50, 6), "YRRROROOOYYRYYRYYRYOOOOO"),
+                Arguments.arguments(LocalTime.of(11, 37, 1), "ORROOROOOYYRYYRYOOOOYYOO")
+        );
+    }
+}
